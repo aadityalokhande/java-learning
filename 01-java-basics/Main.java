14432d077e46f10057ce10aa4ed3face3b7c868f
Aaditya Lokhande <aadityalokhande1308@gmail.com>
2026-08-19 11:13:16 +0530

    Add Main class with introductory print statements

---
+++ 01-java-basics/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My name is Aaditya");
+        System.out.println("I am a computer engineering student");
+        System.out.println("I want to become a software engineer");
+    }
+}
